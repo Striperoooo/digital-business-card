f67026f5476907748b1960b6610933a72bfc3b73
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-09 00:38:46 +0800

    Add Interests component and update styles
    Uncommented and rendered the Interests component in App.jsx. Created Interests.jsx with content and added corresponding styles in index.css for the new component.

---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Interests() {
+    return (
+        <>
+            <div className="interests-container content-padding">
+                <h2 className="heading-style">Interests</h2>
+                <p className="paragraph-style">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>
+        </>
+    )
+}
