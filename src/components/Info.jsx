f139b9eff60d9cf7e07226a8b878f7bc8889ce80
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-08 21:33:39 +0800

    Add Info component UI
    Introduced a new UI for the Info component, including a profile photo, name, title, website link, and styled email/LinkedIn buttons using react-icons. Added corresponding CSS styles in index.css and updated dependencies to include react-icons.

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,28 @@
+import photo from '../assets/photo.JPG';
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+
+
 export default function Info() {
     return (
 
         <header>
+            <img className='photo' src={photo} alt="Portrait of Charles Villanueva" />
+            <div className='name-container'>
+                <h1 className='name'>Charles Deiniel Villanueva</h1>
+                <p className='title'>Frontend Developer</p>
+                <a className='website-link' href="#">charlesvillanueva.website</a>
+            </div>
 
 
+            <div className='link-container'>
+                <a className='btn-link email' href="#">
+                    <MdEmail /> Email
+                </a>
+                <a className='btn-link linkedin' href="#">
+                    <FaLinkedin />LinkedIn
+                </a>
+            </div>
 
         </header>
 
