3d9ff7a1adea0891a7c84bfc2e0489845af98819
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-09 01:06:49 +0800

    Add Footer component with social media icons
    Introduced a new Footer component displaying social media icons and integrated it into the App. Added corresponding CSS styles for the footer layout and icon sizing.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import twitter from '../assets/Twitter icon.png';
+import facebook from '../assets/Facebook Icon.png';
+import instagram from '../assets/Instagram Icon.png';
+import github from '../assets/GitHub Icon.png';
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className='footer-content'>
+                    <a href="">
+                        <img src={twitter} alt="Twitter logo" />
+                    </a>
+                    <a href="">
+                        <img src={facebook} alt="Facebook logo" />
+                    </a>
+                    <a href="">
+                        <img src={instagram} alt="Instagram logo" />
+                    </a>
+                    <a href="">
+                        <img src={github} alt="GitHub logo" />
+                    </a>
+                </div>
+            </footer>
+        </>
+    )
+}
